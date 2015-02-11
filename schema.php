b6e0e6644cba35b1e1fab9774661b4eac1a428b2
Pace Willisson <pace@alum.mit.edu>
2015-02-11 12:05:56 -0500

    db

---
+++ schema.php
@@ -0,0 +1,12 @@
+<?php /* -*- mode:php -*- */
+
+$schema = array ();
+
+$schema[] = array ("name" => "users",
+		   "cols" => array ("user_id" => "integer",
+				    "email" => "longtext"));
+
+
+
+
+
